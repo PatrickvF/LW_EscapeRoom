0f83a0390514d2a0a600c097efa2bbf4df07cacb
PatrickvF <48616645+PatrickvF@users.noreply.github.com>
2019-12-16 14:21:55 +0100

    Player Klasse erstellt nach Klassendiagramm und Basisklasse

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -1,9 +1,73 @@
 package Player;
 
-public class Player {
+import java.util.ArrayList;
+import java.util.List;
 
-	public Player() {
-		// TODO Auto-generated constructor stub
+import Item.Item;
+import start.Creature;
+/**
+ * 
+ * @author patrick.von-fluee
+ * Die Klasse ist eine Spezialisierung der Klasse Creature
+ *
+ */
+public class Player extends Creature implements IBattle
+{
+	/*
+	 * Lege Eine Liste an für die Items die der Player aufnimmt
+	 */
+	private List<Item> foundItems = new ArrayList<>();
+
+	public Player()
+	{
+		super();
 	}
 
+	public Player(String name)
+	{
+		super(name);
+	}
+
+	public Player(int hp)
+	{
+		super(hp);
+	}
+
+	public void addItem(Item item)
+	{
+		foundItems.add(item);
+	}
+
+	public String getName()
+	{
+		return super.getName();
+	}
+
+	public void setName(String name)
+	{
+		super.setName(name);
+	}
+
+	/**
+	 * Getter für die foundItems Liste
+	 * @return
+	 */
+	public List<Item> getFoundItems()
+	{
+		return foundItems;
+	}
+	/**
+	 * Setter für die FoundItems Liste
+	 * @param foundItems
+	 */
+	public void setFoundItems(List<Item> foundItems)
+	{
+		this.foundItems = foundItems;
+	}
+
+	@Override
+	public void takeDamage(int dmg)
+	{
+		super.setHp(super.getHp() - dmg);
+	}
 }
