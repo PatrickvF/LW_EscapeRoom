3ea26d92d95ebf69d68641c1df1787b7f37e846a
PatrickvF <48616645+PatrickvF@users.noreply.github.com>
2019-12-16 15:09:42 +0100

    Die Klasse Game fertiggestellt

--- src/start/Game.java
+++ src/start/Game.java
@@ -41,6 +41,9 @@ public class Game
 			System.out.println("Inkorrekte Eingabe, normal wird ausgewählt");
 			break;
 		}
+
+		}
+
 		gen.createRooms();
 		Walk move = new Walk(gen.rooms);
 		Room currentRoom = gen.rooms[0][0];
@@ -53,7 +56,154 @@ public class Game
 		System.out.println("Du musst aus dem Haus entkommen!!!");
 		System.out.println("Drücke h für die Steuerung.");
 
+		while (exit == 0)
+		{
+			System.out.print("Was willst du machen? ->  ");
+			option = sc.nextLine();
+			switch (option)
+			{
+			case "h":
+			{
+				System.out.println("Du kannst dich durch Räume mit n, e, s, w bewegen.");
+				System.out.println("Du kannst mit c den Raum nach Items durchsuchen.");
+				System.out.println("Deine Items kannst du mit i nachsehen.");
+				System.out.println("Mithilfe von f kannst du nach einem Fluchtweg suchen.");
+				break;
+			}
 
+			case "i":
+			{
+				String allItems = " " + gen.generateItemsToString(player);
+
+				System.out.println("Du hast momentan " + allItems);
+				break;
+			}
+
+			case "f":
+			{
+				if (player.getFoundItems().size() != 0)
+				{
+					if (player.getFoundItems().get(0).getName().equals("Key") && currentRoom.isEnd())
+					{
+						System.out.println("Du hast den Fluchtweg aufgeschlossen und bist geflohen!");
+						exit = 1;
+						sc.close();
+					} else if (player.getFoundItems().get(0).getName().equals("Key") && !currentRoom.isEnd())
+					{
+						System.out.println("Du hast einen Schlüssel, aber hier ist kein Fluchtweg...");
+					}
+				} else
+				{
+					System.out.println("Du hast nichts um fliehen zu können...");
+				}
+				break;
+			}
+
+			case "c":
+			{
+				if (currentRoom.getItem() != null)
+				{
+					System.out.println(player.getName() + " hat einen " + currentRoom.getItem().getName() + "aufgenommen!");
+					player.addItem(currentRoom.getItem());
+					currentRoom.setItem(null);
+				} else
+				{
+					System.out.println("Hier drin ist nichts...");
+				}
+
+				break;
+			}
+			case "n":
+			{
+				newRoom = move.walkNorth(currentRoom);
+				if (newRoom != null)
+				{
+					System.out.println("nach Norden bewegt");
+					currentRoom = newRoom;
+				}
+				else {
+					System.out.println("Da ist keine Türe");
+				}
+				
+					if (currentRoom.getMonster() != null)
+					{
+						System.out.println("Oh nein hier ist eine " + currentRoom.getMonster().getName() + "!");
+					}
+				break;
+			}
+				
+			
+			case "e":
+			{
+				newRoom = move.walkEast(currentRoom);
+				if (newRoom != null)
+				{
+					System.out.println("Nach Osten bewegt");
+					currentRoom = newRoom;
+				}
+				else {
+					System.out.println("Da ist keine Türe");
+				}
+
+					if (currentRoom.getMonster() != null)
+					{
+						System.out.println("Oh nein hier ist eine " + currentRoom.getMonster().getName() + "!");
+					}
+					break;
+				}
+				
+			
+			case "s":
+			{
+				newRoom = move.walkSouth(currentRoom);
+				if (newRoom != null)
+				{
+					System.out.println("Nach Süden bewegt");
+					currentRoom = newRoom;
+				}
+				else {
+					System.out.println("Da ist keine Türe");
+				}
+					if (currentRoom.getMonster() != null)
+					{
+						System.out.println("Oh nein hier ist eine " + currentRoom.getMonster().getName() + "!");
+					}
+					break;
+				}
+				
+			
+			case "w":
+			{
+				newRoom = move.walkWest(currentRoom);
+				if (newRoom != null)
+				{
+					System.out.println("Nach Westen bewegt");
+					currentRoom = newRoom;
+				}
+				else {
+					System.out.println("Da ist keine Türe");
+				}
+					if (currentRoom.getMonster() != null)
+					{
+						System.out.println("Oh nein hier ist eine " + currentRoom.getMonster().getName() + "!");
+					}
+					break;
+				}
+				
+			
+			case "exit":
+			{
+				exit = 1;
+				sc.close();
+				break;
+			}
+			default:
+			{
+				System.out.println("Falsche Eingabe");
+				break;
+			}
+			}
 		}
 	}
+
 }
